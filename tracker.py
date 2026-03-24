f076fc3f9955ae27ccaae77319889b80ac365728
shridhartapakire <shridhartapakire4051@gmail.com>
2026-03-24 19:57:53 +0530

    Added add_expense and view_expenses functionality

--- tracker.py
+++ tracker.py
@@ -16,3 +16,30 @@ def load_data():
 def save_data(data):
     with open(DATA_FILE, "w") as f:
         json.dump(data, f, indent=4)
+
+
+def add_expense(category, amount):
+    data = load_data()
+
+    expense = {
+        "category": category,
+        "amount": amount
+    }
+
+    data.append(expense)
+    save_data(data)
+
+    return "✅ Expense added successfully"
+
+
+def view_expenses():
+    data = load_data()
+
+    if not data:
+        return "No expenses found"
+
+    result = "\n📋 All Expenses:\n"
+    for i, exp in enumerate(data, start=1):
+        result += f"{i}. {exp['category']} - ₹{exp['amount']}\n"
+
+    return result
