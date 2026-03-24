61b1aa835013c866ce5045a81842ac2c85798b22
shridhartapakire <shridhartapakire4051@gmail.com>
2026-03-24 19:58:40 +0530

    Connected main program with expense features

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Expense Tracker Main
 
+from tracker import add_expense, view_expenses
+
+
 def menu():
     print("\n===== EXPENSE TRACKER 💰 =====")
     print("1. Add Expense")
@@ -15,11 +18,30 @@ def main():
         menu()
         choice = input("Enter your choice: ")
 
-        if choice == '4':
+        if choice == '1':
+            category = input("Enter category (food, travel, etc.): ")
+
+            try:
+                amount = float(input("Enter amount: "))
+            except:
+                print("❌ Invalid amount")
+                continue
+
+            msg = add_expense(category, amount)
+            print(msg)
+
+        elif choice == '2':
+            print(view_expenses())
+
+        elif choice == '3':
+            print("Feature coming soon...")
+
+        elif choice == '4':
             print("Exiting...")
             break
+
         else:
-            print("Feature coming soon...")
+            print("❌ Invalid choice")
 
 
 if __name__ == "__main__":
